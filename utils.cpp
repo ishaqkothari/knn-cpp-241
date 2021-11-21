6f9eaf003346c3c2d3a6eb6b09367eef2fdcdce5
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2021-11-21 09:33:37 -0500

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <cmath>
+#include <numeric>
 
+using namespace std;
+
+#include "utils.h"
