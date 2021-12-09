72f3bdf0987c981548b68d57b7e534564067557c
Samuel Topper <stopper@oberlin.edu>
2021-12-08 23:15:24 -0500

    added error and usage methods for knn arguments and file paths

--- main.cpp
+++ main.cpp
@@ -67,8 +67,98 @@ void driver(std::string sys_path_test, std::string sys_path_train, int K, double
   }
 }
 
-int main()
+bool valid_filepath(const std::string & sys_path)
 {
+  std::ifstream test(sys_path);
+  if(!test)
+  {
+    return false;
+  }
+
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+
+if(argc == 2)
+  {
+    if(argv[1][0]== '-' && argv[1][1]== 'h' && argv[1][2] == '\0')
+      {
+        std::cout << "Help Menu \n";
+	std::cout << "KNN cli (2021 Dec 8, compiled " << __TIMESTAMP__ << " " << __TIME__ << ")\n\n";
+        std::cout << "usage: ./knn-cli [train] [test] [options ..]    read in train csv and test csv files from filesystem\n";
+        std::cout << "   or: ./knn-cli -h                             displays help menu\n\n";
+        std::cout << "Arguments:\n";
+        std::cout << "   -h     Displays help menu\n";
+        std::cout << "   -v     Displays output in verbose mode\n";
+	return 0;
+      }
+    else
+      {
+        std::cout << "error.usage ./knn-cli[train][test][options...]\n";
+	std::cout << "enter ./knn-cli -h for a help menu \n";
+	return 1;
+      }
+
+  }
+else if(argc == 4)
+  {
+    if(argv[3][0] == '-' && argv[3][1] == 'v' && argv[3][2] == '\0')
+      {
+        if(valid_filepath(argv[1]) && valid_filepath(argv[2]))
+          {
+            driver(argv[2],argv[1],5,&EuclideanDistance,true);
+            return 0;
+          }
+        else if(!valid_filepath(argv[1]))
+          {
+            std::cout << "Invalid filepath: " << argv[1] << "\n";
+            return 1;
+          } 
+        else if(!valid_filepath(argv[2]))
+          {
+            std::cout << "Invalid filepath: " << argv[2] << "\n";
+            return 1;
+          }	
+      }
+    else
+      {
+        std::cout << "error.usage ./knn-cli[train][test][options...]\n";
+        std::cout << "enter ./knn-cli [train] [test] -v to run program in verbose\n";
+	return 1;
+      }
+  }
+else if(argc == 3)
+  {
+    if(valid_filepath(argv[1]) && valid_filepath(argv[2]))
+      {
+        driver(argv[2],argv[1],5,&EuclideanDistance,false);
+        return 0;
+      }
+    else if(!valid_filepath(argv[1]))
+      {
+        std::cout << "Invalid filepath: " << argv[1] << "\n";
+        return 1;
+      }
+    else if(!valid_filepath(argv[2]))
+      {
+        std::cout << "Invalid filepath: " << argv[2] << "\n";
+        return 1;
+      }
+    else
+      {
+        std::cout << "error.usage ./knn-cli[train][test][options...]\n";
+        std::cout << "enter valid files in the form: knn-cli [train] [test]\n";
+	return 1;
+      }
+  }
+else
+  {
+    std::cout << "error.usage ./knn-cli[train][test][options...]\n";
+    std::cout << "input is not recognized -- please enter in the form: knn-cli[train][test][options...]\n";
+    return 1;
+  }
 
     /* [Iris-virginica] => 0 [Iris-setosa] => 1 [Iris-versicolor] => 2 */
 
