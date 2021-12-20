598df8f12c0d0c5153e7c1dcaf94ff024ad1dd92
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2021-12-20 09:29:58 -0500

    Update knn.cpp

--- knn.cpp
+++ knn.cpp
@@ -1,10 +1,9 @@
-#include "eigen3/Eigen/Geometry"
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include "includes/eigen3/Eigen/Dense"
 #include <fstream>
 #include <algorithm>
+#include "includes/eigen3/Eigen/Dense"
 #include "includes/utils.h"
 
 /* Nathan Englehart, Xuhang Cao, Samuel Topper, Ishaq Kothari (Autumn 2021) */
