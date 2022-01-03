a9c3a000054a2f9448503788f6fb094b4acec1df
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2022-01-03 00:47:53 -0500

    cleaned up split code

--- kfcv.cpp
+++ kfcv.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iterator>
 #include "includes/eigen3/Eigen/Dense"
+#include "includes/eigen3/Eigen/StdVector"
 
 /* Nathan Englehart, Xuhang Cao, Samuel Topper, Ishaq Kothari (Autumn 2021) */
 
@@ -29,25 +30,22 @@ double misclassification_rate(std::vector<int> labels, std::vector<int> ground_t
 std::vector<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>, Eigen::aligned_allocator<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> > > split(Eigen::MatrixXd dataset, int K)
 {
 	
-  /* Returns list of K folds split from input dataset. */
+  	/* Returns list of K folds split from input dataset. */
   
-	int mat_len = dataset.rows(); 
-	int fold_len = mat_len / K;
 	int place = 0;
-	int row_len = dataset.cols();
 
 	std::vector<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>, Eigen::aligned_allocator<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> > > list; // does not like not regular ints as arguments, e.g. row len and fold len	
 
 	for(int i = 0; i < K; i++)
 	{
-		Eigen::MatrixXd fold(fold_len,row_len); // may be reverse 
-    
+		Eigen::MatrixXd fold(dataset.rows() / K,dataset.cols());
+
 		for(int j = 0; j < fold_len; j++)
 		{
 			Eigen::VectorXd x = dataset.row(place++);
 			fold.row(j) = x;
 		}
-    
+
 		list.push_back(fold);
 	}
 
